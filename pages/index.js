bead5c03b3dfd649acba5870728287007d2c3e14
Ian Perez <iperez@mit.edu>
2021-02-05 01:38:03 -0400

    Added a trending shows section

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import classes from '../styles/PosterList.module.css'
 import {Button, ButtonBase, Container, Typography} from "@material-ui/core";
 import Image from 'next/image';
 import axios from 'axios';
@@ -9,11 +8,13 @@ export async function getStaticProps(context){
     const base = 'https://api.themoviedb.org/3';
     const popularPromise = axios.get(base + "/tv/popular", {params: {api_key: process.env.API_KEY}})
     const topRatedPromise = axios.get(base + "/tv/top_rated", {params: {api_key: process.env.API_KEY}})
-    const [popularShows, topRatedShows] = await Promise.all([popularPromise, topRatedPromise]);
+    const trendingPromise = axios.get(base + '/trending/tv/day', {params: {api_key: process.env.API_KEY}})
+    const [popularShows, topRatedShows, trendingShows] = await Promise.all([popularPromise, topRatedPromise, trendingPromise]);
     return {
         props: {
-            popularShows: popularShows.data,
-            topRatedShows: topRatedShows.data,
+            popularShows: popularShows?.data,
+            topRatedShows: topRatedShows?.data,
+            trendingShows: trendingShows?.data,
         },
         revalidate: 450,
     }
@@ -23,14 +24,16 @@ export default function Home(props) {
     const image_path = 'https://image.tmdb.org/t/p/w220_and_h330_face/'
     let popularShows = props?.popularShows?.results ?? [];
     let topRatedShows = props?.topRatedShows?.results ?? [];
+    let trendingShows = props?.trendingShows?.results ?? [];
   return (
     <div>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-        <Container className={classes.container}>
+        <Container style={{padding: '10px'}}>
             <PosterList title={"Popular Shows"} shows={popularShows} />
+            <PosterList title={"Trending Shows"} shows={trendingShows} />
             <PosterList title={'Top Rated Shows'} shows={topRatedShows} />
         </Container>
     </div>
